aa37d05d1d83e2294e75ffcace564338e46cd712
Athos Montarroyos <athosg05@gmail.com>
2024-11-06 21:14:35 -0300

    aula 1 completa

---
+++ curso2/src/components/BarraLateral.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent, } from 'vue';
+export default defineComponent ({
+name: 'App'
+
+})
+</script>
+
+
+<template>
+
+<header>
+    <h1>
+        <img src="../assets/logo.png" alt="logo do Alura Tracker">
+    </h1>
+</header>
+
+
+</template>
+
+
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
