0c7314afe7065505f17f363bce4d8d0ee060aca6
Athos Montarroyos <athosg05@gmail.com>
2024-11-08 19:47:20 -0300

    terminei aula 3, começando aula 4

---
+++ curso2/src/components/Box.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot>
+        
+    </slot>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent ({
+    name: 'BoxFormulario'
+})
+</script>
+
+<style scoped>
+.box {
+    background: #bb2929; 
+}
+</style>
--- curso2/src/components/Formulario.vue
+++ curso2/src/components/Formulario.vue
@@ -23,6 +23,7 @@ import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
     name: "FormularioCronometro",
+    emits: ['aoSalvarTarefa'],
     components: {
         Temporizador
     },
@@ -33,8 +34,14 @@ export default defineComponent({
     },
     methods: {
         finalizarTarefa (tempoDecorrido: number) : void{
-            console.log('tempo da tarefa', tempoDecorrido)
-            console.log('descrição da tarefa', this.descricao)
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao
+            }) 
+               
+                
+
+            
             this.descricao = ''
         
         }
