9c8b4b699cdbcea99bb497aa00c93c599cac14a3
Athos Montarroyos <athosg05@gmail.com>
2024-11-10 15:16:30 -0300

    a

--- curso2/src/views/Projetos/Formulario.vue
+++ curso2/src/views/Projetos/Formulario.vue
@@ -1,74 +1,63 @@
 <template>
-    <section class="projetos">
-        <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomoDoProjeto" class="label">
-                    Nome Do Projeto
-                </label>
-                <input type="text" class="input" v-model="nomeDoProjeto" id="NomeDoProjet">
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>
+    <section>
+      <form @submit.prevent="salvar">
+        <div class="field">
+          <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+          <input
+            type="text"
+            class="input"
+            v-model="nomeDoProjeto"
+            id="nomeDoProjet"
+          />
+        </div>
+        <div class="field">
+          <button class="button" type="submit">Salvar</button>
+        </div>
+      </form>
     </section>
-</template>
-
-<script lang="ts">
-import { useStore } from '@/store';
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-    name: 'FormularioProjetos',
+  </template>
+  
+  <script lang="ts">
+  import { useStore } from "@/store";
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "FormularioProjeto",
     props: {
-        id: {
-            type: String,
-        },
+      id: {
+        type: String
+      }
     },
-    mounted() {
-        if (this.id) {
-            const projeto = this.store.state.projetos.find(proj => proj.id === this.id)
-            console.log(projeto)
-            this.nomeDoProjeto = projeto?.nome || '';
-        }
+    mounted () {
+      if(this.id) {
+        const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
+        this.nomeDoProjeto = projeto?.nome || ''
+      }
     },
     data() {
-        return {
-            nomeDoProjeto: "",
-        };
+      return {
+        nomeDoProjeto: ""
+      };
     },
     methods: {
-        salvar() {
-            if (this.id) {
-                console.log(this.id)
-                this.store.commit('ALTERA_PROJETO', {
-                    id: this.id,
-                    nome: this.nomeDoProjeto
-                }
-                )
-            } else {
-                console.log(this.id)
-                this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-            }
-            this.nomeDoProjeto = '';
-            this.$router.push('/projetos')
+      salvar() {
+        if (this.id) {
+          this.store.commit('ALTERA_PROJETO', {
+            id: this.id,
+            nome: this.nomeDoProjeto
+          })
+        } else {
+          this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
         }
+        this.nomeDoProjeto = "";
+        this.$router.push('/projetos')
+      },
     },
-    setup() {
-        const store = useStore()
-        return {
-            store,
-        }
+    setup () {
+      const store = useStore()
+      return {
+        store
+      }
     }
-})
-
-</script>
-
-<style scoped>
-.projetos {
-    padding: 1.25rem;
-}
-</style>
+  });
+  </script>
